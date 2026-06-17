d3e00bbc9b67e92ec5c38ee400b785085f3411a0
Vlad Rafeenko <rafeenkovlad@gmail.com>
2026-06-17 23:30:40 +0300

    Import report.functions to include in build

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,6 +2,8 @@ import { useCallback, useState } from "react";
 import { createFileRoute } from "@tanstack/react-router";
 import { queryOptions, useQuery } from "@tanstack/react-query";
 import { getReport, type InspectionElement } from "@/lib/report.api";
+// Import to ensure backward-compatible server function is included in the build
+import "@/lib/report.functions";
 import { ElementViewer } from "@/components/ElementViewer";
 import { SchemaTabs } from "@/components/SchemaTabs";
 import { MediaGallery } from "@/components/MediaGallery";
